5375354ac7cb2d5e4f0591d546b6e2d134461cf5
Chachanidze29 <chachanidze29m@gmail.com>
2022-06-13 00:29:42 +0400

    created booklist and book detail view and template

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from catalog.Models.book import Book
 from catalog.Models.bookinstance import BookInstance
 from catalog.Models.author import Author
+from django.views import generic
 
 
 def index(request):
@@ -18,3 +19,23 @@ def index(request):
     }
 
     return render(request, 'index.html', context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    template_name = 'booklist.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(BookListView, self).get_context_data(**kwargs)
+        context['book_list'] = Book.objects.all()
+        return context
+
+
+class BookDetail(generic.DetailView):
+    model = Book
+    template_name = 'book.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(BookDetail, self).get_context_data(**kwargs)
+        context['book'] = get_object_or_404(Book, pk=self.kwargs['pk'])
+        return context
