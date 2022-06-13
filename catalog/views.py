fec4de0e9b82e09d73536284bdd1f2bf81df79cc
Chachanidze29 <chachanidze29m@gmail.com>
2022-06-13 11:59:15 +0400

    added author list and detail views and templates

--- catalog/views.py
+++ catalog/views.py
@@ -31,11 +31,31 @@ class BookListView(generic.ListView):
         return context
 
 
-class BookDetail(generic.DetailView):
+class BookDetailView(generic.DetailView):
     model = Book
     template_name = 'book.html'
 
     def get_context_data(self, **kwargs):
-        context = super(BookDetail, self).get_context_data(**kwargs)
+        context = super(BookDetailView, self).get_context_data(**kwargs)
         context['book'] = get_object_or_404(Book, pk=self.kwargs['pk'])
+        return context
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    template_name = 'authorList.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(AuthorListView, self).get_context_data(**kwargs)
+        context['author_list'] = Author.objects.all()
+        return context
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Book
+    template_name = 'author.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(AuthorDetailView, self).get_context_data(**kwargs)
+        context['author'] = get_object_or_404(Author, pk=self.kwargs['pk'])
         return context
